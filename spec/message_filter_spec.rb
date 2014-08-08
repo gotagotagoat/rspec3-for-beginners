b3b59e05e0c392f1425d78a1bdcf9aa281809fd6
gotagotagoat <gotagotagoat@gmail.com>
2014-08-08 23:59:14 +0900

    use subject

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -5,16 +5,11 @@ RSpec.describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
-  # it 'detects message with NG word' do
-  #   expect(@filter).to be_detect('hello from foo')
-  # end
+  subject { @filter }
   it {
-    expect(@filter).to be_detect('hello from foo')
+    is_expected.to be_detect('hello from foo')
   }
-  # it 'does not detect message without NG word' do
-  #   expect(@filter).not_to be_detect('hello, world')
-  # end
   it {
-    expect(@filter).not_to be_detect('hello, world')
+    is_expected.not_to be_detect('hello, world')
   }
 end
