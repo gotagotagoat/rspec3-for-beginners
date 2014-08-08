fb0cdf3cc7281f23d51be5a8d13a6dbe09fcaf7f
gotagotagoat <gotagotagoat@gmail.com>
2014-08-09 00:00:47 +0900

    remove instance variable

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 require_relative '../app/message_filter'
 
 RSpec.describe MessageFilter, 'with argument "foo"' do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
-  subject { @filter }
+  subject { MessageFilter.new('foo') }
   it {
     is_expected.to be_detect('hello from foo')
   }
