295d7e4303ce3aff9f46b804eb0e336a897454f9
gotagotagoat <gotagotagoat@gmail.com>
2014-08-08 23:57:02 +0900

    add description to describe

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 require_relative '../app/message_filter'
 
-RSpec.describe MessageFilter do
+RSpec.describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
-  it 'detects message with NG word' do
+  # it 'detects message with NG word' do
+  #   expect(@filter).to be_detect('hello from foo')
+  # end
+  it {
     expect(@filter).to be_detect('hello from foo')
-  end
-  it 'does not detect message withoutw NG word' do
+  }
+  # it 'does not detect message without NG word' do
+  #   expect(@filter).not_to be_detect('hello, world')
+  # end
+  it {
     expect(@filter).not_to be_detect('hello, world')
-  end
+  }
 end
