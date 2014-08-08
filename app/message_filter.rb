48084f443f7617acbdcc6ed91b76fb460ab12614
gotagotagoat <gotagotagoat@gmail.com>
2014-08-08 22:47:39 +0900

    add first spec

--- app/message_filter.rb
+++ app/message_filter.rb
@@ -1,2 +1,4 @@
 class MessageFilter
+  def
+  end
 end
--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
-RSpec.describe "MessageFilter" do
+RSpec.describe 'MessageFilter' do
+  it 'detects message with NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to eq true
+  end
 end
