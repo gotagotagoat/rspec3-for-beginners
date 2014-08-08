071800668c52f12866232bb5fcc60ccf98f4394b
gotagotagoat <gotagotagoat@gmail.com>
2014-08-09 00:01:47 +0900

    join lines

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -3,10 +3,6 @@ require_relative '../app/message_filter'
 
 RSpec.describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it {
-    is_expected.to be_detect('hello from foo')
-  }
-  it {
-    is_expected.not_to be_detect('hello, world')
-  }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world') }
 end
