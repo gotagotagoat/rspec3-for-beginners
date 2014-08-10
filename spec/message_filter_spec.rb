f9d2c1d6e39e91a8dee2f3557ab6bdba87d2dd16
gotagotagoat <gotagotagoat@gmail.com>
2014-08-10 17:59:41 +0900

    nest describe blocks

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 require_relative '../app/message_filter'
 
-RSpec.shared_examples 'MessageFilter with argument "foo"' do
-  it { is_expected.to be_detect('hello from foo') }
-  it { is_expected.not_to be_detect('hello, world') }
-end
+RSpec.describe MessageFilter do
+  shared_examples 'MessageFilter with argument "foo"' do
+    it { is_expected.to be_detect('hello from foo') }
+    it { is_expected.not_to be_detect('hello, world') }
+  end
 
-RSpec.describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo') }
-  it_behaves_like 'MessageFilter with argument "foo"'
-end
+  context 'with argument "foo"' do
+    subject { MessageFilter.new('foo') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
 
-RSpec.describe MessageFilter, 'with argument "foo", "bar"' do
-  subject { MessageFilter.new('foo', 'bar') }
-  it { is_expected.to be_detect('hello from bar') }
-  it_behaves_like 'MessageFilter with argument "foo"'
+  context 'with argument "foo", "bar"' do
+    subject { MessageFilter.new('foo', 'bar') }
+    it { is_expected.to be_detect('hello from bar') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
 end
