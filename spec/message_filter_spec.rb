6358d6e8630d7ebf7ea00fc2e472e5d73b29eb66
gotagotagoat <gotagotagoat@gmail.com>
2014-08-10 21:24:04 +0900

    move its-examples

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,20 +1,17 @@
 require 'spec_helper'
+require 'rspec/its'
 require_relative '../app/message_filter'
 
 RSpec.describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
     it { is_expected.to be_detect('hello from foo') }
     it { is_expected.not_to be_detect('hello, world') }
+    its(:ng_words) { is_expected.not_to be_empty }
   end
 
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
-    it { expect(subject.ng_words).not_to be_empty }
-
-    # it 'ng_words should not be empty' do
-    #   expect(subject.ng_words).not_to be_empty
-    # end
   end
 
   context 'with argument "foo", "bar"' do
